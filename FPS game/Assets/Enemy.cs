e9981f3bb67c1dd2c2652f82137b69155d9e6ee1
Miguel <Miguel.h279@stu.nebo.edu>
2022-12-13 14:17:59 -0700

    AI

---
+++ FPS game/Assets/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Ling;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       public int curHP;
+       public int maxHP;
+       public int scoreToGive;
+       
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
